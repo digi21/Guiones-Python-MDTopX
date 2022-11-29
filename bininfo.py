9c84a94c329580e65fc619285c83b2145fb60854
José Ángel <joseangelmt@outlook.com>
2022-11-29 12:50:19 +0100

    Simplificada la lógica utilizando el objeto Counter

--- bininfo.py
+++ bininfo.py
@@ -1,4 +1,5 @@
 from mdtopx import *
+from collections import Counter
 
 def bininfo():
     '''
@@ -17,42 +18,15 @@ def bininfo():
     if view is None or type(view) != drawingView:
         raise Exception("La ventana abierta no es una ventana de dibujo")
 
-    cantidad = {
-        "lineas": 0,
-        "lineas-eliminadas": 0,
-        "textos": 0,
-        "textos-eliminados": 0,
-        "complejos": 0,
-        "complejos-eliminados": 0,
-        "clotoides": 0,
-        "clotoides-eliminadas": 0
-    }
+    noEliminadas = Counter(type(x) for x in view if not x.deleted)
+    eliminadas = Counter(type(x) for x in view if x.deleted)
 
-    for g in view:
-        if type(g) is line:
-            if g.deleted:
-                cantidad["lineas-eliminadas"] += 1
-            else:
-                cantidad["lineas"] += 1
-        elif type(g) is text:
-            if g.deleted:
-                cantidad["textos-eliminados"] += 1
-            else:
-                cantidad["textos"] += 1
-        elif type(g) is complex:
-            if g.deleted:
-                cantidad["complejos-eliminados"] += 1
-            else:
-                cantidad["complejos"] += 1
-        elif type(g) is clotoid:
-            if g.deleted:
-                cantidad["clotoides"] += 1
-            else:
-                cantidad["clotoides-eliminadas"] += 1
+    def imprime(titulo, tipo, noEliminadas, eliminadas):
+        print("{}: {} de las cuales {} eliminadas".format(titulo, noEliminadas[tipo] + eliminadas[tipo], eliminadas[tipo]))
 
-    print("Líneas: {} de las cuales {} eliminadas".format(cantidad["lineas"] + cantidad["lineas-eliminadas"], cantidad["lineas-eliminadas"]))
-    print("Textos: {} de las cuales {} eliminados".format(cantidad["textos"] + cantidad["textos-eliminados"], cantidad["textos-eliminados"]))
-    print("Complejos: {} de las cuales {} eliminados".format(cantidad["complejos"] + cantidad["complejos-eliminados"], cantidad["complejos-eliminados"]))
-    print("Clotoides: {} de las cuales {} eliminados".format(cantidad["clotoides"] + cantidad["clotoides-eliminadas"], cantidad["clotoides-eliminadas"]))
+    imprime("Líneas", line, noEliminadas, eliminadas)
+    imprime("Textos", text, noEliminadas, eliminadas)
+    imprime("Complejos", complex, noEliminadas, eliminadas)
+    imprime("Clotoides", clotoid, noEliminadas, eliminadas)
 
 bininfo()
